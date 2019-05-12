783a5d2d8f57f47befe80a6e72edaac5d438eb00
ahmetcangir <ahmet@statixdesign.com>
2019-05-13 00:40:44 +0200

    Solved question 2

--- main.py
+++ main.py
@@ -28,7 +28,14 @@ query_1 = """select articles.title, count(*) as num
     limit 3;"""
 
 # 2. Who are the most popular article authors of all time?
-query_2 = ""
+query_2 = """select authors.name, count(*) as num
+    from articles, authors, log
+    where log.status='200 OK'
+    and authors.id = articles.author
+    and articles.slug = substr(log.path, 10)
+    group by authors.name
+    order by num desc;
+    """
 
 # 3. On which days did more than 1% of requests lead to errors?
 query_3 = ""
@@ -42,5 +49,14 @@ def top_three_articles():
         print(" - \"{}\" -- {} views".format(title, num))
 
 
+def top_authors():
+    '''Print the most popular article authors of all time.'''
+    print("The most popular article authors of all time:")
+    query = query_db(query_2)
+    for name, num in query:
+        print(" - {} -- {} views".format(name, num))
+
+
 if __name__ == '__main__':
     top_three_articles()
+    top_authors()
