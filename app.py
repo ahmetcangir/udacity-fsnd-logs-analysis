a9b29eba6deb633081d046f31e00e6ced23bb19d
ahmetcangir <ahmet@statixdesign.com>
2019-05-13 02:03:21 +0200

    Solved question 2

--- app.py
+++ app.py
@@ -38,7 +38,30 @@ query_2 = """select authors.name, count(*) as num
     """
 
 # 3. On which days did more than 1% of requests lead to errors?
-query_3 = ""
+query_3 = """select error_rates.day, error_rates.rate AS error_rate
+    from (
+        select day, cast(error_count as real) / cast(total_count as real) as rate
+            from (
+                select http_requests_count.day,
+                    http_requests_count.count as total_count,
+                    http_error_counts.count as error_count
+                    from
+                        (
+                            select date_trunc('day', log.time) as day, count(*)
+                            from log
+                            group by day
+                        ) as http_requests_count,
+                        (
+                            select date_trunc('day', log.time) as day, count(*)
+                            from log
+                            where log.status similar to '(4|5)%' -- 400, 500 series status codes
+                            group by day
+                        ) as http_error_counts
+                where http_requests_count.day = http_error_counts.day
+          ) as daily_http_request_counts
+       ) as error_rates
+    where error_rates.rate > 0.01;
+    """
 
 
 def top_three_articles():
@@ -57,6 +80,15 @@ def top_authors():
         print(" - {} -- {} views".format(name, num))
 
 
+def top_error_days():
+    query = query_db(query_3)
+    '''Print the error days with more than %1 of requests.'''
+    print("The days on which more than 1% of requests lead to errors:")
+    for day, rate in query:
+        print(" - {0:%B %d, %Y} — {1:.2%} views".format(day, rate))
+
+
 if __name__ == '__main__':
     top_three_articles()
     top_authors()
+    top_error_days()
